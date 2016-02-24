d1d48f253ee68bf2c3744fb1203bd422d4774ca1
Evgeniy Kuzmin <jekccs@gmail.com>
2016-02-24 23:20:51 +0200

    spec let into command

--- spec/Gamma/FixturesBoostBundle/Command/FixturesBoostCommandSpec.php
+++ spec/Gamma/FixturesBoostBundle/Command/FixturesBoostCommandSpec.php
@@ -4,11 +4,32 @@ namespace spec\Gamma\FixturesBoostBundle\Command;
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Symfony\Component\Console\Input\InputInterface;
+use Gamma\FixturesBoostBundle\Service\FixturesBoostService;
 
 class FixturesBoostCommandSpec extends ObjectBehavior
 {
+    function let(InputInterface $input)
+    {
+        $input->bind(Argument::cetera())->willReturn();
+        $input->isInteractive()->willReturn(false);
+        $input->validate()->willReturn();
+        $input->getOption('clear')->willReturn(FixturesBoostService::CLEAR_MODE_SCHEMA);
+    }
+
     function it_is_initializable()
     {
         $this->shouldHaveType('Gamma\FixturesBoostBundle\Command\FixturesBoostCommand');
     }
+
+    function it_is_a_container_aware_command()
+    {
+        $this->shouldHaveType('Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand');
+    }
+
+    function it_has_a_name()
+    {
+        $this->getName()->shouldReturn('gamma:fixtures:boostload');
+    }
+
 }
